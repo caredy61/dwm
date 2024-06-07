c969a7c466e408e163512e3d03701e36ddca0c5b
Anton Hagel <anton.hagel@gmail.com>
2024-06-07 22:38:04 +0200

    Changed color scheme of HUD, added volume inputs.

--- config.h
+++ config.h
@@ -6,19 +6,24 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *up_vol[]   = { "amixer", "-c", "0", "set", "Master", "1dB+", "unmute", NULL };
+static const char *down_vol[] = { "amixer", "-c", "0", "set", "Master", "1dB-", "unmute",  NULL };
+static const char *mute_vol[] = { "amixer", "-c", "0", "set", "Master", "mute", NULL };
 static const char *brighter[] = { "brightnessctl", "set", "10%+", NULL };
 static const char *dimmer[] = { "brightnessctl", "set", "10%-", NULL };
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_green[]       = "#00d914";
+static const char col_red[]         = "#d80000";
+//static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_red },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_green  },
 };
 
 /* tagging */
@@ -56,12 +61,12 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/usr/local/bin/st", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/usr/local/bin/alacritty", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -90,6 +95,9 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
         { 0, XF86XK_MonBrightnessDown, spawn, {.v = dimmer} },
         { 0, XF86XK_MonBrightnessUp, spawn, {.v = brighter} },
+	{ 0, XF86XK_AudioMute,        spawn, {.v = mute_vol } },
+       	{ 0, XF86XK_AudioLowerVolume, spawn, {.v = down_vol } },
+       	{ 0, XF86XK_AudioRaiseVolume, spawn, {.v = up_vol } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
